eef4c9cf928b4ff94c9782b64d5f844c02d48b12
Aleksey Cheusov <vle@gmx.net>
2016-06-12 18:00:22 +0300

    Fix compilation failure on NetBSD-6

--- compatlib/mkc_macro.h
+++ compatlib/mkc_macro.h
@@ -10,11 +10,11 @@
  */
 #define __arraycount(__x)       (sizeof(__x) / sizeof(__x[0]))
 
-#ifdef HAVE_MEMBER_STRUCT_STAT_ST_MTIM_SYS_STAT_H
+#ifdef HAVE_MEMBER_STRUCT_STAT_ST_MTIMESPEC_SYS_STAT_H
 #  define timecmp(f1, f2, cmp)                                   \
-        (((f1)->st_mtim.tv_sec == (f2)->st_mtim.tv_sec) ?        \
-            ((f1)->st_mtim.tv_nsec cmp (f2)->st_mtim.tv_nsec) :  \
-            ((f1)->st_mtim.tv_sec cmp (f2)->st_mtim.tv_sec))
+        (((f1)->st_mtimespec.tv_sec == (f2)->st_mtimespec.tv_sec) ?        \
+            ((f1)->st_mtimespec.tv_nsec cmp (f2)->st_mtimespec.tv_nsec) :  \
+            ((f1)->st_mtimespec.tv_sec cmp (f2)->st_mtimespec.tv_sec))
 #else
 #  ifdef HAVE_MEMBER_STRUCT_STAT_ST_MTIME_SYS_STAT_H
 #    define timecmp(f1, f2, cmp) f1.st_mtime cmp f2.st_mtime
