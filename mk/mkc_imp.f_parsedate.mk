e23a2fa01c71ad154cb51174a3efe49fe3936324
Aleksey Cheusov <vle@gmx.net>
2017-03-30 11:05:27 +0300

    Untested support for parsedate(3)

---
+++ mk/mkc_imp.f_parsedate.mk
@@ -0,0 +1,18 @@
+# Copyright (c) 2016 by Aleksey Cheusov
+#
+# See LICENSE file in the distribution.
+
+.ifndef _MKC_IMP_F_PARSEDATE_MK
+_MKC_IMP_F_PARSEDATE_MK := 1
+
+MKC_CHECK_FUNCS3   =	parsedate:util.h
+MKC_CHECK_FUNCLIBS =	parsedate:util
+
+.include <mkc_imp.conf-cleanup.mk>
+
+. if ${HAVE_FUNCLIB.parsedate:U} == 0 && \
+    ${.CURDIR:T} == "compatlib"
+SRCS +=	${SRCDIR_compatlib}/mkc_parsedate.y
+. endif
+
+.endif #_MKC_IMP_F_PARSEDATE_MK
