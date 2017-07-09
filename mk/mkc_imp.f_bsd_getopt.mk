61d3eaa6e4ca41a18311a975acd6a7af2b7aa8b5
Aleksey Cheusov <vle@gmx.net>
2017-07-09 20:59:27 +0300

    Fix build failure under NetBSD

--- mk/mkc_imp.f_bsd_getopt.mk
+++ mk/mkc_imp.f_bsd_getopt.mk
@@ -9,12 +9,12 @@ MKC_CHECK_DEFINES   =	__GLIBC__:string.h
 
 .include <mkc_imp.conf-cleanup.mk>
 
-.if ${.CURDIR:T} == "compatlib"
-. if ${HAVE_DEFINE.__GLIBC__.string_h:U} == 1
+.if ${HAVE_DEFINE.__GLIBC__.string_h:U} == 1
+. if ${.CURDIR:T} == "compatlib"
 SRCS     +=	mkc_bsd_getopt.c
+. endif
 .else
 CPPFLAGS +=	-DHAVE_POSIX_GETOPT=1
-. endif
 .endif
 
 .endif #_MKC_IMP_F_BSD_GETOPT_MK
