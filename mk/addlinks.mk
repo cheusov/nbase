526f8e03930117cffb021707c55617383c2f7b8f
Aleksey Cheusov <vle@gmx.net>
2017-07-09 19:38:37 +0300

    LPREFIX variable was introduced

---
+++ mk/addlinks.mk
@@ -0,0 +1,30 @@
+.ifdef LINKSPREFIX
+.include <mkc.init.mk>
+
+LBINDIR  ?=	${BINDIR:S/${PREFIX}/${LINKSPREFIX}/}
+LSBINDIR ?=	${SBINDIR:S/${PREFIX}/${LINKSPREFIX}/}
+LMANDIR  ?=	${MANDIR:S/${PREFIX}/${LINKSPREFIX}/}
+
+.for p in ${PROGNAME:U${PROGS}}
+SYMLINKS +=	${BINDIR}/${p} ${LBINDIR}/nb-${p}
+.endfor
+
+.for s in ${SCRIPTS}
+SYMLINKS +=	${BINDIR}/${SCRIPTSNAME_${s}:U${s}} ${LBINDIR}/nb-${SCRIPTSNAME_${s}:U${s}}
+.endfor
+
+.for from to in ${LINKS:U}
+SYMLINKS +=	${from:S,^${DESTDIR},,} ${LBINDIR}/nb-${to:T}
+.endfor
+
+.for m in ${MAN:U${PROG}.1}
+SYMLINKS +=	${MANDIR}/man${m:E}/${m} ${LMANDIR}/man${m:E}/nb-${m}
+.endfor
+
+.for m mto in ${MLINKS}
+SYMLINKS +=	${MANDIR}/man${m:E}/${m} ${LMANDIR}/man${m:E}/nb-${mto}
+.endfor
+
+.endif # INKSPREFIX
+
+.include <mkc.mk>
