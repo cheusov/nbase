cff8fdc5b67e9514dc48f2c5a2d467bbfb474f8c
Aleksey Cheusov <vle@gmx.net>
2018-03-04 23:54:20 +0300

    feature base64: fix the problem seen on OpenBSD-5.3

--- mk/mkc_imp.f_base64.mk
+++ mk/mkc_imp.f_base64.mk
@@ -5,20 +5,24 @@
 .ifndef _MKC_IMP_F_BASE64_MK
 _MKC_IMP_F_BASE64_MK := 1
 
+old :=			${MKC_COMMON_HEADERS}
+MKC_COMMON_HEADERS =	netinet/in.h # for OpenBSD 5.3
+
 MKC_CHECK_FUNCS4   =	b64_ntop:resolv.h
 MKC_CHECK_FUNCS3   =	b64_pton:resolv.h
 
-MKC_CHECK_FUNCLIBS +=	b64_ntop:resolv b64_pton:resolv
+MKC_CHECK_FUNCLIBS +=	main:resolv
+#MKC_CHECK_FUNCLIBS +=	b64_ntop:resolv b64_pton:resolv
 
 .include <mkc_imp.conf-cleanup.mk>
 
-.if ${.CURDIR:T} == "compatlib"
-. if ( ${HAVE_FUNCLIB.b64_ntop:U} != 1 && ${HAVE_FUNCLIB.b64_ntop.resolv:U} != 1) \
-  || ( ${HAVE_FUNCLIB.b64_pton:U} != 1 && ${HAVE_FUNCLIB.b64_pton.resolv:U} != 1)
+MKC_COMMON_HEADERS :=   ${old}
+.undef old
+
+.if ${HAVE_FUNC4.b64_ntop.resolv_h:U} == 1 && ${HAVE_FUNC3.b64_pton.resolv_h:U} == 1
+CPPFLAGS += -DHAVE_B64_FUNCS
+.elif ${.CURDIR:T} == "compatlib"
 SRCS +=	mkc_base64.c
-. elif ${HAVE_FUNCLIB.b64_ntop.resolv:U} != 1 || ${HAVE_FUNCLIB.b64_pton.resolv:U} != 1)
-LDADD +=	-lresolv
-. endif
 .endif
 
 .endif #_MKC_IMP_F_BASE64_MK
