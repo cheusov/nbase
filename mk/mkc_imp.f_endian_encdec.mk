c02fbd705154136654d3a54958b2acbffa0748d2
Aleksey Cheusov <vle@gmx.net>
2018-03-09 22:01:50 +0200

    xinstall, cksum, mtree: fix a typo in mk/mkc_imp.f_endian_encdec.mk => build succeeds on DragopnFlyBSD

--- mk/mkc_imp.f_endian_encdec.mk
+++ mk/mkc_imp.f_endian_encdec.mk
@@ -7,7 +7,7 @@ _MKC_IMP_F_ENDIAN_ENCDEC_MK := 1
 funcs1 =	be16dec be32dec be64dec le16dec le32dec le64dec
 funcs2 =	be16enc be32enc be64enc le16enc le32enc le64enc
 
-MKC_CHECK_HEADER    =	sys/endian.h
+MKC_CHECK_HEADERS   =	sys/endian.h
 MKC_CHECK_FUNCS1   :=	${funcs1:S|$|:sys/endian.h|}
 MKC_CHECK_FUNCS2   :=	${funcs2:S|$|:sys/endian.h|}
 MKC_CHECK_FUNCLIBS :=	${funcs1} ${funcs2}
