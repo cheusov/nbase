006d3dab09c941dd17a105924694afe4cf929898
Aleksey Cheusov <vle@gmx.net>
2019-03-27 17:35:21 +0300

    ul tabs qsubst: fix build failure on OpenSuSE Leap 0.15

---
+++ mk/mkc_imp.f_curses.mk
@@ -0,0 +1,39 @@
+# Copyright (c) 2019 by Aleksey Cheusov
+#
+# See LICENSE file in the distribution.
+
+.ifndef _MKC_IMP_CURSES_MK
+_MKC_IMP_CURSES_MK := 1
+
+MKC_REQUIRE_HEADERS +=	curses.h
+
+.include <mkc_imp.conf-cleanup.mk>
+
+.if ${HAVE_HEADER.curses_h:U} == 1
+
+LIB_CURSES   ?=	curses
+LIB_NCURSES  ?=	ncurses
+
+MKC_CHECK_DEFINES  +=	NCURSES_VERSION:curses.h
+
+.include <mkc_imp.conf-cleanup.mk>
+
+.if ${HAVE_DEFINE.NCURSES_VERSION.curses_h:U} == 1
+libcurses=${LIB_NCURSES}
+.else
+libcurses=${LIB_CURSES}
+.endif
+
+MKC_CHECK_FUNCLIBS +=	main:${libcurses}
+
+.include <mkc_imp.conf-cleanup.mk>
+
+.if ${HAVE_FUNCLIB.main.${libcurses}}
+LDADD +=	-l${libcurses}
+.endif
+
+#CPPFLAGS +=	-D_MKC_CHECK_CURSES
+
+.endif # HAVE_HEADER.curses_h
+
+.endif #_MKC_IMP_CURSES_MK
