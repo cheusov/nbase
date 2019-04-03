8be22d1c25904b71d370c50a15c5eb81cbfc55a9
Aleksey Cheusov <vle@gmx.net>
2019-04-03 21:58:21 +0000

    Fix a typo in mk/mkc_imp.f_macro.mk

--- mk/mkc_imp.f_macro.mk
+++ mk/mkc_imp.f_macro.mk
@@ -6,7 +6,7 @@
 _MKC_IMP_F_MACRO_MK := 1
 
 MKC_CHECK_TYPES         +=	u_quad_t:sys/types.h
-MKC_CHECK_HEADERS_FILES +=	sys/sysmacros.h sys/cdefs.h
+MKC_CHECK_HEADER_FILES  +=	sys/sysmacros.h sys/cdefs.h
 
 CPPFLAGS +=	-D_MKC_CHECK_MACRO
 
