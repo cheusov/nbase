5cb060f7198b9e7ed5eadc25f8b583cb8dc7705a
Aleksey Cheusov <vle@gmx.net>
2019-04-16 23:04:21 +0300

    On Darwin we also use our own implementation of getbsize(3) for the same reason

--- mk/hacks.mk
+++ mk/hacks.mk
@@ -1,7 +1,7 @@
 .ifndef _HACKS_MK_
 _HACKS_MK_ := 1
 
-.if ${OPSYS:U} == FreeBSD || ${OPSYS:U} == OpenBSD
+.if ${OPSYS:U} == FreeBSD || ${OPSYS:U} == OpenBSD || ${OPSYS:U} == Darwin
 # On FreeBSD getbsize(3) doesn't accept NULL as the first argument, so we
 # always use our own version.
 HAVE_FUNCLIB.getbsize=0
