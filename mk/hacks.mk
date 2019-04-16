86cc275056b694cdcde80ba96b81632d43545887
Aleksey Cheusov <vle@gmx.net>
2019-04-16 19:56:48 +0300

    On OpenBSD we also use our own implementation of getbsize(3) for the same reason

--- mk/hacks.mk
+++ mk/hacks.mk
@@ -1,7 +1,7 @@
 .ifndef _HACKS_MK_
 _HACKS_MK_ := 1
 
-.if ${OPSYS:U} == FreeBSD
+.if ${OPSYS:U} == FreeBSD || ${OPSYS:U} == OpenBSD
 # On FreeBSD getbsize(3) doesn't accept NULL as the first argument, so we
 # always use our own version.
 HAVE_FUNCLIB.getbsize=0
