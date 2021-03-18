76bd1593602dc7e9ff75cd19a86ff0f67d5037bb
Aleksey Cheusov <vle@gmx.net>
2021-03-19 00:01:04 +0300

    nawk: add portable (well, you know...) implementation for fpurge

---
+++ compatlib/mkc_fpurge.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright © 2021 Aleksey Cheusov <vle@gmx.net>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
+ * THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef _MKC_FPURGE_H_
+#define _MKC_FPURGE_H_ 1
+
+#ifndef _MKC_CHECK_FPURGE
+# error "Missing MKC_FEATURES += fpurge"
+#endif
+
+#if HAVE_FUNC1_FPURGE_STDIO_H
+#include <stdio.h>
+#elif HAVE_FUNC1___FPURGE_STDIO_EXT_H
+#include <stdio.h>
+#include <stdio_ext.h>
+#define fpurge(f) (__fpurge(f), 0)
+#else
+#error "Can find neither fpurge(3) nor __fpurge(3), sorry"
+#endif
+
+#endif
