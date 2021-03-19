96efffd2073df5288bf9b94710824f3497ffca54
Aleksey Cheusov <vle@gmx.net>
2021-03-19 19:00:17 +0300

    m4: fix build failure on Linux

---
+++ mk/imp.f_strtoi.mk
@@ -0,0 +1,21 @@
+# Copyright (c) 2021 by Aleksey Cheusov
+#
+# See LICENSE file in the distribution.
+
+.ifndef _MKC_IMP_F_STRTOI_MK
+_MKC_IMP_F_STRTOI_MK := 1
+
+MKC_CHECK_FUNCS6   +=	strtoi:inttypes.h
+MKC_CHECK_FUNCLIBS +=	strtoi
+
+.include <mkc.conf.mk>
+
+.if ${HAVE_FUNCLIB.strtoi:U} != 1
+. if ${.CURDIR:T} == "compatlib"
+SRCS +=	mkc_strtoi.c
+. endif
+.endif
+
+CPPFLAGS +=	-D_MKC_CHECK_STRTOI
+
+.endif #_MKC_IMP_F_STRTOI_MK
