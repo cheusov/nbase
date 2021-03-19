ee75afe4785a4836bbe73590709f2163ac038af7
Aleksey Cheusov <vle@gmx.net>
2021-03-20 00:42:26 +0300

    Remove check for siggetmask(3) from bsd_signal feature, it is not necessary

--- mk/imp.f_bsd_signal.mk
+++ mk/imp.f_bsd_signal.mk
@@ -6,7 +6,7 @@
 _MKC_IMP_F_BSD_SIGNAL_MK := 1
 
 MKC_CHECK_FUNCS1   +=	sigblock:signal.h sigsetmask:signal.h sigmask:signal.h
-MKC_CHECK_FUNCS0   +=	siggetmask:signal.h
+# MKC_CHECK_FUNCS0   +=	siggetmask:signal.h
 MKC_FUNC_OR_DEFINE.siggetmask =	yes
 MKC_FUNC_OR_DEFINE.sigsetmask =	yes
 MKC_FUNC_OR_DEFINE.sigblock   =	yes
@@ -14,8 +14,8 @@ MKC_FUNC_OR_DEFINE.sigmask    =	yes
 
 .include <mkc.conf.mk>
 
-.if ${.CURDIR:T} == "compatlib"
-. if ${HAVE_FUNC1.sigblock.signal_h:U} != 1 || ${HAVE_FUNC1.sigsetmask.signal_h:U} != 1 || ${HAVE_FUNC0.siggetmask.signal_h:U} != 1
+.if ${HAVE_FUNC1.sigblock.signal_h:U} != 1 || ${HAVE_FUNC1.sigsetmask.signal_h:U} != 1 # || ${HAVE_FUNC0.siggetmask.signal_h:U} != 1
+. if ${.CURDIR:T} == "compatlib"
 SRCS +=	mkc_bsd_signal.c
 . endif
 .endif
