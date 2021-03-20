b9a6aaa74eee6e72c941e244630c6cff155d5b50
Aleksey Cheusov <vle@gmx.net>
2021-03-20 17:58:19 +0300

    mtree: fix build on NetBSD (fix HAVE_STRUCT_STAT_ST_FLAGS define)

--- mtree/nbtool_config.h
+++ mtree/nbtool_config.h
@@ -2,7 +2,7 @@
 #define _NBTOOL_CONFIG_H_
 
 #if HAVE_MEMBER_STRUCT_STAT_ST_FLAGS_SYS_STAT_H
-# define HAVE_STRUCT_STAT_ST_FLAGS
+# define HAVE_STRUCT_STAT_ST_FLAGS 1
 #endif
 
 #include "mkc_lchmod.h"
