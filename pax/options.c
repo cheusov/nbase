27af3f6038713e0e5c02ee0b5830416cbadc94a8
Aleksey Cheusov <vle@gmx.net>
2021-03-23 16:35:14 +0300

    pax: fix build failure on OpenSuSE (multiple definition of chdname variable)

--- pax/options.c
+++ pax/options.c
@@ -89,6 +89,7 @@ static int nopids;		/* tar mode: suppress "pids" for -p option */
 static char flgch[] = FLGCH;	/* list of all possible flags (pax) */
 static OPLIST *ophead = NULL;	/* head for format specific options -x */
 static OPLIST *optail = NULL;	/* option tail */
+char *chdname;
 
 static int opt_add(const char *);
 static int no_op(void);
