a076170d6e84cdd9f5c9febed909f3059cdf7c1b
Aleksey Cheusov <vle@gmx.net>
2024-05-24 17:30:22 +0200

    pax: fix compilation failure on SunOS/gcc -m32

--- pax/ar_io.c
+++ pax/ar_io.c
@@ -457,7 +457,7 @@ ar_close(void)
 	/* mimic cpio's block count first */
 	if (frmt && strcmp(NM_CPIO, argv0) == 0) {
 		(void)fprintf(listf, OFFT_F " blocks\n",
-		    (rdcnt ? rdcnt : wrcnt) / 5120);
+		    (uintmax_t)(rdcnt ? rdcnt : wrcnt) / 5120);
 	}
 
 	ar_summary(0);
